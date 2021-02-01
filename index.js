7d15199d0b697726552b945df65fc8d4b345c225
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-01-31 22:08:53 -0500

    commit tree 466dc28505ce69bb7cd42320889ad252d2f5398c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+
+const driver = {}
+
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    let new_obj = Object.create({}, driver);
+    new_obj.push({key : value})
+    return new_obj
+}
+
+// function createNewMenu (oldMenu, menuChanges) {
+//     return Object.assign({}, oldMenu, menuChanges);
+//   }
